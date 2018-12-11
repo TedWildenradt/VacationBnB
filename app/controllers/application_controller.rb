c9ca060438ee9870cec77c620f404fd42bde7010
TedWildenradt <tedkwildenradt@gmail.com>
2018-12-11 11:31:22 -0800

    Application controller for user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :require_login, :logged_in?
+
+  attr_reader :current_user
+
+  def login(user)
+    user.reset_session_token
+    session[:session_token] = user.session_token
+    @current_user = user
+
+  end
+
+  def logout
+    current_user.reset_session_token
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def require_login
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+  validates :email, :password_digest, :session_token, :fname, :lname, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+end
