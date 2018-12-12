68fade35056fc38238a78bfabeec0585931afc80
TedWildenradt <tedkwildenradt@gmail.com>
2018-12-12 13:33:16 -0800

    Seed database with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  User.destroy_all
+
+  User.create(email: 'demo@email.com', password: 'password', fname: 'Demo', lname: 'Login')
+  User.create(email: 'ron@email.com', password: 'password', fname: 'Ron', lname: 'Burgandy')
+  User.create(email: 'ricky@email.com', password: 'password', fname: 'Ricky', lname: 'Bobby')
+  User.create(email: 'michael@email.com', password: 'password', fname: 'Michael', lname: 'Scott')
+  User.create(email: 'rick@email.com', password: 'password', fname: 'Rick', lname: 'Sanchez')
+end 
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -24,7 +24,7 @@ class SessionForm extends React.Component {
   demoUser(e){
     e.preventDefault()
     this.props.action({
-      email: "number1spy@m16.com",
+      email: "demo@email.com",
       password: "password"
     });
   }
