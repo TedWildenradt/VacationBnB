e3588a28d2a31b7f822f85751fc6f8ccdf2e8f12
TedWildenradt <tedkwildenradt@gmail.com>
2018-12-16 10:29:38 -0800

    Create google map and render with home index

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -12,13 +12,14 @@ class Search extends React.Component{
   render() {
 
     const {homes, fetchHomes} = this.props;
+    
     return(
       <div className="homes-map-container">
         <div className="homes-map-index">
           <HomeIndex homes={homes} fetchHomes={fetchHomes}/>
         </div>
         <div className="homes-map-map">
-          <HomeMap />
+          <HomeMap homes={homes}/>
         </div>
       </div>
     )
