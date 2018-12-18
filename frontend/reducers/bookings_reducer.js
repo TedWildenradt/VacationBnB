d0068fd9d0c8d9590172daed1e0150f3a144a959
TedWildenradt <tedkwildenradt@gmail.com>
2018-12-18 11:33:50 -0800

    Create booking reducer

--- frontend/reducers/bookings_reducer.js
+++ frontend/reducers/bookings_reducer.js
@@ -5,12 +5,17 @@ const BookingsReducer = (state={}, action) => {
   Object.freeze(state)
   switch (action.type) {
     case RECEIVE_ALL_BOOKINGS:
-    
+      return action.bookings
     case RECEIVE_BOOKING:
-    
+      let newBooking = {[action.booking.id]: action.booking}
+      return merge({}, state, newBooking);
     case REMOVE_BOOKING:
-
+      let newState = merge({}, state);
+      delete newState[action.bookingId]
+      return newState;
     default:
       return state;
   }
 }
+
+export default BookingsReducer;
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import usersReducer from './users_reducer';
 import homesReducer from './homes_reducer';
+import BookingsReducer from './bookings_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
-  homes: homesReducer
+  homes: homesReducer,
+  bookings: BookingsReducer
 });
 
 export default entitiesReducer;
