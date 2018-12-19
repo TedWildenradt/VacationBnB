1297237e9c03cb126cf7465ee02efb6abc58d1fb
TedWildenradt <tedkwildenradt@gmail.com>
2018-12-19 09:40:30 -0800

    css for booking form

--- frontend/components/booking/booking_form.jsx
+++ frontend/components/booking/booking_form.jsx
@@ -32,14 +32,14 @@ class BookingForm extends React.Component{
       <div className="booking-form-container">
         <form className="booking-form" onSubmit={this.handleSubmit}>
           
-          <h2>${home.price}</h2>
+          <h2>${home.price} <span id="per-night">per night</span></h2>
           
           <hr/>
           
           <div className="booking-form-dates">
             <h5>Dates</h5>
           </div>
-          
+          <div className="date-range-picker">
           <DateRangePicker
           startDate={this.state.start_date} // momentPropTypes.momentObj or null,
           startDateId="start_date_id" // PropTypes.string.isRequired,
@@ -48,20 +48,21 @@ class BookingForm extends React.Component{
           onDatesChange={({ startDate, endDate }) => this.setState({ start_date: startDate, end_date: endDate })}
           focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
           onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-          startDatePlaceholderText="Start"
-          endDatePlaceholderText="End"
+          startDatePlaceholderText="Check in"
+          endDatePlaceholderText="Check out"
+          numberOfMonths={1}
           />
-
+          </div>
           <div className="booking-form-guests">
             <h5>Guests</h5>
           </div>
           <div className="booking-form-guest-input">
-            <input className="booking-form-input" type="text"/>
+            <input id="guests-button" className="booking-form-input" type="text" placeholder={`${home.num_guests} guests`}/>
           </div>
           <div className="booking-form-book">
             <input id="booking-button"className="booking-form-input" type="submit" value="Book"/>
           </div>
-          <div>
+          <div id="no-charge">
             <h6>You won't be charged yet</h6>
           </div>
           <hr/>
