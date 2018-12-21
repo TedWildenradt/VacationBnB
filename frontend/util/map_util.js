e0637b0d50da4f32985e2a2b9dfa68bff077ce25
TedWildenradt <tedkwildenradt@gmail.com>
2018-12-20 16:22:26 -0800

    Add map centers for LA and NY

--- frontend/util/map_util.js
+++ frontend/util/map_util.js
@@ -5,6 +5,16 @@ const getSFLocation = () => ({
   zoom: 12
 })
 
+const getLALocation = () => ({
+  center: { lat: 34.052836, lng: -118.246883 },
+  zoom: 9
+})
+
+const getNYLocation = () => ({
+  center: { lat: 40.783963, lng:  -73.968029 },
+  zoom: 12
+})
+
 const standardOptions = () => ({
     center: { lat: 39.012435, lng: -101.434000 }, // this is USA
     zoom: 3
@@ -12,10 +22,13 @@ const standardOptions = () => ({
 
 
 const getLocationDetails = (query) => {
-  switch (query) {
+  switch (query.toLowerCase()) {
     case 'san francisco':
       return getSFLocation();
-
+    case 'los angeles':
+      return getLALocation();
+    case 'new york':
+      return getNYLocation();
     default:
       return standardOptions();
   }
